ae8afe0649cf63e5a7e359f0e443c49187f8bf69
FontusMoiseSchamma <141655719+FontusMoiseSchamma@users.noreply.github.com>
2023-08-12 20:24:04 -0400

    Add files via upload

---
+++ Battle_1.py
@@ -0,0 +1,14 @@
+a = "a"
+i = "i"
+s=0
+l=0
+entre = input("entre yon teks :")
+
+for let in entre:
+    if(let ==a):
+        s= s+1
+    if(let ==i ): 
+        l = l+1   
+print("teks ou a gen",s, '(a) ladanl epi ',l,'(i )ladanl')
+
+
